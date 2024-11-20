9e0904409f18aae97186ab4200e42a8341bf1c89
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-11-20 13:34:12 +0800

    update

--- docs/scripts/targetrank.js
+++ docs/scripts/targetrank.js
@@ -11,8 +11,6 @@ miss = 0;
 
 const targetScore = 1007500;
 
-let totalScore = calculateScore(justiceCritical, justice, attack, miss);
-
 function calculateScore(justiceCritical, justice, attack, miss) {
     const baseScore = 1000000 / totalNotes; 
     const scoreJusticeCritical = baseScore * 1.01 * justiceCritical;
@@ -24,6 +22,7 @@ function calculateScore(justiceCritical, justice, attack, miss) {
 let bestJusticeCritical1 = 0;
 let bestJustice1 = totalNotes;
 let bestAttack1 = 0;
+let totalScore = calculateScore(justiceCritical, justice, attack, miss);
 
 while (justice > 0) {
     totalScore = calculateScore(justiceCritical, justice, attack, miss);
@@ -54,8 +53,27 @@ bestJusticeCritical1 = justiceCritical;
 bestJustice1 = justice;
 bestAttack1 = attack;
 
-const targetScore2 = 1009000;
+if (totalScore < targetScore) {
+    while (attack > 0 && totalScore < targetScore) {
+        attack--;
+        justice += 50;
+        justiceCritical -= 49;
+        totalScore = calculateScore(justiceCritical, justice, attack, miss);
+        if (totalScore >= targetScore) break;
 
+        while (justice > 0 && totalScore < targetScore) {
+            justice--;
+            justiceCritical++;
+            totalScore = calculateScore(justiceCritical, justice, attack, miss);
+        }
+    }
+
+    bestJusticeCritical1 = justiceCritical;
+    bestJustice1 = justice;
+    bestAttack1 = attack;
+}
+
+const targetScore2 = 1009000;
 justiceCritical = 0;
 justice = totalNotes;
 attack = 0;
@@ -93,9 +111,28 @@ bestJusticeCritical2 = justiceCritical;
 bestJustice2 = justice;
 bestAttack2 = attack;
 
+if (totalScore2 < targetScore2) {
+    while (attack > 0 && totalScore2 < targetScore2) {
+        attack--;
+        justice += 50;
+        justiceCritical -= 49;
+        totalScore2 = calculateScore(justiceCritical, justice, attack, miss);
+        if (totalScore2 >= targetScore2) break;
+
+        while (justice > 0 && totalScore2 < targetScore2) {
+            justice--;
+            justiceCritical++;
+            totalScore2 = calculateScore(justiceCritical, justice, attack, miss);
+        }
+    }
+    bestJusticeCritical2 = justiceCritical;
+    bestJustice2 = justice;
+    bestAttack2 = attack;
+}
+
 alert(
     "最低要求：\n" + 
     "SSS    : ( " + bestJusticeCritical1 + "-" + bestJustice1 + "-" + bestAttack1 + "-0 ) : " + Math.round(totalScore) + "\n" + 
     "SSS+ : ( " + bestJusticeCritical2 + "-" + bestJustice2 + "-" + bestAttack2 + "-0 ) : " + Math.round(totalScore2) +
     "\n\n1 Miss = 2 Attack = 100 Justice"
-); 
+);
