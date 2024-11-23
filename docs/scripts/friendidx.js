bb34e86acfc5bd72eba6247c32e080d811e09b54
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-11-24 02:21:45 +0800

    Update friendidx

--- docs/scripts/friendidx.js
+++ docs/scripts/friendidx.js
@@ -2,16 +2,28 @@ async function displayFriendIdx() {
     const friendForms = document.querySelectorAll(".friend_btn_block form");
     const idxInput = friendForms[0].querySelector("input[name='idx']");
     const resultText = idxInput.value.trim();
-
     const userChoice = confirm(`Friend Code：${resultText}\n是否複製？Copy?`);
     if (userChoice) {
-        setTimeout(async () => {
+        if (navigator.clipboard && navigator.clipboard.write) {
             try {
-                await navigator.clipboard.writeText(resultText);
+                const blob = new Blob([resultText], { type: "text/plain" });
+                const clipboardItem = new ClipboardItem({ "text/plain": blob });
+                await navigator.clipboard.write([clipboardItem]);
             } catch (err) {
                 alert(`Error：${err.message}`);
             }
-        }, 0);
+        } else {
+            const tempInput = document.createElement("textarea");
+            tempInput.value = resultText;
+            document.body.appendChild(tempInput);
+            tempInput.select();
+            try {
+                document.execCommand("copy");
+            } catch (err) {
+                alert(`Error：${err.message}`);
+            }
+            document.body.removeChild(tempInput);
+        }
     }
 }
 displayFriendIdx();
