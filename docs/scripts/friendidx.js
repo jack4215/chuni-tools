361e2a20285a00e94757033223534023adfa3921
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-11-24 02:04:04 +0800

    Update friendidx

--- docs/scripts/friendidx.js
+++ docs/scripts/friendidx.js
@@ -1,26 +1,19 @@
-function displayFriendIdx() {
+async function displayFriendIdx() {
     const friendForms = document.querySelectorAll(".friend_btn_block form");
-    const idxInput = friendForms[0].querySelector("input[name='idx']");
+    const idxInput = friendForms[0]?.querySelector("input[name='idx']")
     const idxValue = idxInput.value;
     const userChoice = confirm(`Friend Code：${idxValue}\n是否複製？Copy?`);
+
     if (userChoice) {
-        if (navigator.clipboard && navigator.clipboard.writeText) {
-            navigator.clipboard.writeText(idxValue).catch(err => {
-                alert(err.message);
-            });
-        } else {
-            try {
-                const tempTextarea = document.createElement("textarea");
-                tempTextarea.value = idxValue;
-                document.body.appendChild(tempTextarea);
-                tempTextarea.select();
-                const successful = document.execCommand("copy");
-                document.body.removeChild(tempTextarea);
-                if (!successful) throw new Error("Error");
-            } catch (err) {
-                alert(err.message);
-            }
+        try {
+            const resultText = await Promise.resolve(idxValue);
+            setTimeout(() => {
+                navigator.clipboard.writeText(resultText).catch(err => {
+                    alert(`Error：${err.message}`);
+                });
+            }, 0);
+        } catch (err) {
+            alert(`Error：${err.message}`);
         }
     }
 }
-displayFriendIdx();
