84375b89cccba2943ee02b2cf863ea19f3afbc6e
Jack Lu <87149267+jack4215@users.noreply.github.com>
2024-12-17 18:18:03 +0800

    update NV

--- docs/friendrecord-viewer/main.bundle.js
+++ docs/friendrecord-viewer/main.bundle.js
@@ -957,28 +957,19 @@
         Nt.updateConstData(), Ht.updateConstData(), At.updateConstData()
       })),
       nt = Ne("showOverPower", "hide", ["hide", "value", "percentage", "dgvalue", "dgpercentage"]),
-      rt = function(e, t, n = (() => {})) {
-        let r = localStorage.getItem(e);
-        (null === r || "true" !== r && "false" !== r) && (r = JSON.stringify(t), localStorage.setItem(e, r));
-        let o = JSON.parse(r);
-        const {
-          subscribe: s,
-          set: a,
-          update: i
-        } = Ce(o);
+      rt = function (e, t, n = (() => {})) {
+        localStorage.setItem(e, JSON.stringify(false));
+        const o = false;
         return {
-          subscribe: s,
-          set(t) {
-            a(t), localStorage.setItem(e, JSON.stringify(t)), n(t)
+          subscribe: (run) => {
+            run(o);
+            return { unsubscribe: () => {} };
           },
-          update: i,
-          reset() {
-            this.set(t)
-          },
-          toggle() {
-            this.update((e => !e))
-          }
-        }
+          set: () => {},
+          update: () => {},
+          reset: () => {},
+          toggle: () => {} 
+        };
       }("showPlaycount", !1),
       ot = {
         manual: Number.POSITIVE_INFINITY
