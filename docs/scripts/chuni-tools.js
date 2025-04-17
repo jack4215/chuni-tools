86a7b5bb76b9d5fdc45319b1a45819c271355b34
Jack Lu <87149267+jack4215@users.noreply.github.com>
2025-04-17 10:19:45 +0800

    update

--- docs/scripts/chuni-tools.js
+++ docs/scripts/chuni-tools.js
@@ -40,7 +40,8 @@
             const resultContainer = document.createElement("div");
             resultContainer.className = "chuni-tools-container";
             resultContainer.innerHTML = `
-                <p><a href="https://chuni.tsaibee.org" target="_blank" style="color: #75b4f7;text-decoration: none;">CHUNITHM Tools</a></p>
+                <p><a href="https://chuni.tsaibee.org" target="_blank" style="color: #75b4f7;text-decoration: none;">CHUNITHM Tools</a></p><br>
+                <p>Rating 計算確認中，請稍後</p>
                 <hr width="96%">
                 <p style="font-size:15px;">使用即同意本服務之<a href="https://chuni.tsaibee.org/privacy-policy" target="_blank">隱私權政策</a></p>
                 <p style="font-size:15px;">By using this service, you agree to its <a href="https://chuni.tsaibee.org/privacy-policy" target="_blank">Privacy Policy</a>.</p>
