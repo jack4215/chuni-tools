aa4fcd94a508925d5aaa8697f48a4b7b2021cf7a
Jack Lu <87149267+jack4215@users.noreply.github.com>
2025-04-20 18:48:48 +0800

    Fix

--- docs/scripts/b30image.js
+++ docs/scripts/b30image.js
@@ -29,9 +29,7 @@ function switchNumToDiff(n) {
 
 // https://qiita.com/h53/items/05139982c6fd81212b08
 function toISOStringWithTimezone(date) {
-    const pad = function (str) {
-        return ('0' + str).slice(-2);
-    };
+    const pad = (str) => (`0${str}`).slice(-2);
     const year = (date.getFullYear()).toString();
     const month = pad((date.getMonth() + 1).toString());
     const day = pad(date.getDate().toString());
@@ -47,23 +45,20 @@ function toISOStringWithTimezone(date) {
 }
 
 async function main() {
-
-    
     // https://note.affi-sapo-sv.com/js-sleep.php
     const sleep = waitTime => new Promise(resolve => setTimeout(resolve, waitTime));
 
-    const VERSION = "1.0.0";
+    const VERSION = "2.0.0";
     const difficulties = ["Basic", "Advanced", "Expert", "Master", "Ultima"];
     const engMode = location.hostname === "chunithm-net-eng.com";
     let baseUrl = engMode ? "https://chunithm-net-eng.com/" : "https://new.chunithm-net.com/chuni-mobile/html/";
     baseUrl += "mobile/";
-    const homeUrl = baseUrl + "home/";
-    const musicUrl = baseUrl + "record/musicGenre/";
-    const sendUrl = musicUrl + "send";
-    const ratingsUrlBase = homeUrl + "playerData/";
-    const ratingsBestUrl = ratingsUrlBase + "ratingDetailBest/"; // ベスト枠
-    const ratingsRecentUrl = ratingsUrlBase + "ratingDetailRecent/"; // リーセント枠
-    const ratingsNextUrl = ratingsUrlBase + "ratingDetailNext/"; // 候補楽曲
+    const homeUrl = `${baseUrl}home/`;
+    const musicUrl = `${baseUrl}record/musicGenre/`;
+    const sendUrl = `${musicUrl}send`;
+    const ratingsUrlBase = `${homeUrl}playerData/`;
+    const ratingsBestUrl = `${ratingsUrlBase}ratingDetailBest/`; // ベスト枠
+    const ratingsNewUrl = `${ratingsUrlBase}ratingDetailRecent/`; // 新曲枠（URL上はリーセント枠と変化なし）
     const playerData = {};
 
     // location check
@@ -89,37 +84,60 @@ async function main() {
     UiBase.style.padding = "10px";
     UiBase.style.borderRadius = "10px";
     UiBase.style.width = "min(90%, 400px)";
-    UiBase.style.height = "min(90%, 500px)";
+    UiBase.style.height = "fit-content";
     UiBase.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
     UiBase.style.overflowX = "hidden";
     UiBase.style.textAlign = "left";
-    UiBase.innerHTML = `<p>CHUNITHM Score Data Downloader v${VERSION}</p><p>by <a href="https://reiwa.f5.si/" target="_blank">音ゲーツール置き場</a></p><hr>`;
-    UiBase.innerHTML += engMode ? "<p>Fetching player's profiles...</p>" : "<p>プレイヤープロフィールを取得しています...</p>";
+    UiBase.innerHTML = `<p>CHUNITHM Score Data Downloader v${VERSION}</p>`;
+    UiBase.innerHTML += `<p>by <a href="https://reiwa.f5.si/" target="_blank">${engMode ? "Qman's Tools Square" : "音ゲーツール置き場"}</a></p><hr>`;
+    const progressElement = UiBase.appendChild(document.createElement("div"));
+
+    const fullProgressCount = 8;
+    let progressCount = 1;
+    progressElement.innerHTML = engMode ? `<p>(${String(progressCount)}/${String(fullProgressCount)}) Fetching player's profiles...</p>` : `<p>(${String(progressCount)}/${String(fullProgressCount)}) プレイヤープロフィールを取得しています...</p>`;
 
     await sleep(1000);
 
     // Player Data
-    const honor = homeDoc.querySelector(".player_honor_text").textContent;
+    const honors = homeDoc.querySelectorAll(".player_honor_text");
+    const mainHonor = honors[0].textContent;
+    let subHonor1 = null;
+    let subHonor2 = null;
+    if (honors.length > 1) {
+        subHonor1 = honors[1].textContent;
+    }
+    if (honors.length > 2) {
+        subHonor2 = honors[2].textContent;
+    }
     const name = homeDoc.querySelector(".player_name_in").textContent;
     const ratingBlockImgs = homeDoc.querySelectorAll(".player_rating_num_block img");
     const ratingStr = [];
     for (let i = 0; i < ratingBlockImgs.length; i++) {
-        let src = ratingBlockImgs[i].src;
+        const src = ratingBlockImgs[i].src;
         if (src.includes("comma")) {
             ratingStr.push(".");
             continue;
         }
-        let match = src.match(/([0-9])\.png/);
+        const match = src.match(/([0-9])\.png/);
         if (match) ratingStr.push(match[1]);
     }
     const rating = Number(ratingStr.join(""));
-    const ratingMax = Number(homeDoc.querySelector(".player_rating_max").textContent);
+    let playerLevel = Number(homeDoc.querySelector(".player_lv").innerText);
+    const playerReborn = homeDoc.querySelector(".player_reborn");
+    if (playerReborn !== null) {
+        playerLevel += Number(playerReborn.innerText) * 100;
+    }
+    const lastPlayed = homeDoc.querySelector(".player_lastplaydate_text").textContent; // format: yyyy/MM/DD hh:mm
 
-    playerData.honor = honor;
+    playerData.appVersion = VERSION;
+    playerData.honor = mainHonor;
+    playerData.subHonor1 = subHonor1;
+    playerData.subHonor2 = subHonor2;
     playerData.name = name;
     playerData.rating = rating;
-    playerData.ratingMax = ratingMax;
+    playerData.level = playerLevel;
     playerData.updatedAt = toISOStringWithTimezone(new Date());
+    playerData.lastPlayed = toISOStringWithTimezone(new Date(lastPlayed))
 
     const musicData = [];
 
@@ -134,14 +152,15 @@ async function main() {
     formData.set("token", token);
 
     for (let i = 0; i < difficulties.length; i++) {
-        let difficulty = difficulties[i];
-        UiBase.innerHTML += engMode ? `<p>Fetching ${difficulty.toUpperCase()} data...</p>` : `<p>${difficulty.toUpperCase()}のデータを取得しています...</p>`;
-        let musicDoc = await fetchMusicFormDoc(sendUrl + difficulty, token);
+        progressCount++;
+        const difficulty = difficulties[i];
+        progressElement.innerHTML = engMode ? `<p>(${String(progressCount)}/${String(fullProgressCount)}) Fetching ${difficulty.toUpperCase()} data...</p>` : `<p>(${String(progressCount)}/${String(fullProgressCount)}) ${difficulty.toUpperCase()}のデータを取得しています...</p>`;
+        const musicDoc = await fetchMusicFormDoc(sendUrl + difficulty, token);
 
-        let forms = musicDoc.querySelectorAll("form");
+        const forms = musicDoc.querySelectorAll("form");
         for (let j = 0; j < forms.length; j++) {
             if (forms[j].querySelector(".music_title") === null) continue;
-            let title = forms[j].querySelector(".music_title").textContent.trim();
+            const title = forms[j].querySelector(".music_title").textContent.trim();
             if (!title) continue;
 
             let score = 0;
@@ -150,28 +169,37 @@ async function main() {
             }
             let isAllJustice = false;
             let isFullCombo = false;
-            let lampImgsDiv = forms[j].querySelector(".play_musicdata_icon");
+            let fullChainLv = 0; // 0: 未フルチェイン, 1: 白金フルチェイン, 2: 金フルチェイン
+            const lampImgsDiv = forms[j].querySelector(".play_musicdata_icon");
             if (lampImgsDiv !== null) {
-                let lampImgs = forms[j].querySelector(".play_musicdata_icon").querySelectorAll("img");
+                const lampImgs = forms[j].querySelector(".play_musicdata_icon").querySelectorAll("img");
                 for (let k = 0; k < lampImgs.length; k++) {
-                    let src = lampImgs[k].src;
+                    const src = lampImgs[k].src;
+
                     if (src.includes("alljustice")) {
                         isAllJustice = true;
                         isFullCombo = true;
-                        break;
                     } else if (src.includes("fullcombo")) {
                         isFullCombo = true;
-                        break;
+                    }
+
+                    if (src.includes("fullchain2")) {
+                        fullChainLv = 2;
+                    } else if (src.includes("fullchain")) {
+                        fullChainLv = 1;
                     }
                 }
             };
+            const idx = Number.parseInt(forms[j].querySelector('input[type="hidden"][name="idx"]').getAttribute("value"));
 
             musicData.push({
                 title: title,
                 difficulty: difficulty,
                 score: score,
                 isAllJustice: isAllJustice,
-                isFullCombo: isFullCombo
+                isFullCombo: isFullCombo,
+                fullChainLv: fullChainLv,
+                idx: idx
             });
         }
 
@@ -180,123 +208,137 @@ async function main() {
 
     // レーティング対象楽曲: AJ/FCの照合のためmusicDataを使う
     // ベスト枠
-    UiBase.innerHTML += engMode ? "<p>Fetching best songs...</p>" : "<p>ベスト枠楽曲を取得しています...</p>";
+    progressCount++;
+    progressElement.innerHTML = engMode ? `<p>(${String(progressCount)}/${String(fullProgressCount)}) Fetching best songs...</p>` : `<p>(${String(progressCount)}/${String(fullProgressCount)}) ベスト枠楽曲を取得しています...</p>`;
     const ratingsBestDoc = await fetchPageDoc(ratingsBestUrl);
     const bestMusics = ratingsBestDoc.getElementsByTagName("form");
     const bestMusicData = [];
     for (let i = 0; i < bestMusics.length; i++) {
-        let music = bestMusics[i];
-        let title = music.querySelector(".music_title").textContent.trim();
-        let difficulty = switchNumToDiff(Number(music.querySelector('input[type="hidden"][name="diff"]').getAttribute("value")));
-        let score = Number(music.querySelector(".play_musicdata_highscore").querySelector("span").textContent.replaceAll(",", ""));
+        const music = bestMusics[i];
+        const title = music.querySelector(".music_title").textContent.trim();
+        const difficulty = switchNumToDiff(Number(music.querySelector('input[type="hidden"][name="diff"]').getAttribute("value")));
+        const score = Number(music.querySelector(".play_musicdata_highscore").querySelector("span").textContent.replaceAll(",", ""));
 
         // AJ/FCの照合
         let isAllJustice = false;
         let isFullCombo = false;
-        let musicInMusicData = musicData.filter(m => m.title === title && m.difficulty === difficulty && m.score === score)[0];
+        let fullChainLv = 0;
+        const musicInMusicData = musicData.filter(m => m.title === title && m.difficulty === difficulty && m.score === score)[0];
         if (musicInMusicData) {
             isAllJustice = musicInMusicData.isAllJustice;
             isFullCombo = musicInMusicData.isFullCombo;
+            fullChainLv = musicInMusicData.fullChainLv;
         }
+        const idx = Number.parseInt(music.querySelector('input[type="hidden"][name="idx"]').getAttribute("value"));
 
         bestMusicData.push({
             title: title,
             difficulty: difficulty,
             score: score,
             isAllJustice: isAllJustice,
-            isFullCombo: isFullCombo
+            isFullCombo: isFullCombo,
+            fullChainLv: fullChainLv,
+            idx: idx
         });
     }
     await sleep(1000);
 
-    // "Stardust:RAY" 
-    if (bestMusicData.length < 30) {
-        UiBase.innerHTML += engMode
-            ? `<p>Missing some songs. Filling with "Stardust:RAY".</p>`
-            : `<p>不足的歌曲將以「Stardust:RAY」填補。</p>`;
-        
-        let userScore;
-        do {
-            userScore = prompt(`請輸入「Stardust:RAY」的分數：`);
-            userScore = Number(userScore);
-        } while (!Number.isInteger(userScore) || userScore <= 0);
-
-        bestMusicData.push({
-            title: "Stardust:RAY",
-            difficulty: "Master",
-            score: userScore,
-            isAllJustice: false,
-            isFullCombo: false
-        });
-    }
-
-    // リーセント枠
-    UiBase.innerHTML += engMode ? "<p>Fetching recent songs...</p>" : "<p>リーセント枠楽曲を取得しています...</p>";
-    const ratingsRecentDoc = await fetchPageDoc(ratingsRecentUrl);
-    const recentMusics = ratingsRecentDoc.getElementsByTagName("form");
-    const recentMusicData = [];
-    for (let i = 0; i < recentMusics.length; i++) {
-        let music = recentMusics[i];
-        let title = music.querySelector(".music_title").textContent.trim();
-        let difficulty = switchNumToDiff(Number(music.querySelector('input[type="hidden"][name="diff"]').getAttribute("value")));
-        let score = Number(music.querySelector(".play_musicdata_highscore").querySelector("span").textContent.replaceAll(",", ""));
-
-        recentMusicData.push({
-            title: title,
-            difficulty: difficulty,
-            score: score,
-        });
-    }
-    await sleep(1000);
-
-    // 候補楽曲
-    UiBase.innerHTML += engMode ? "<p>Fetching best-candidate songs...</p>" : "<p>ベスト枠候補楽曲を取得しています...</p>";
-    const ratingsNextDoc = await fetchPageDoc(ratingsNextUrl);
-    const nextMusics = ratingsNextDoc.getElementsByTagName("form");
-    const nextMusicData = [];
-    for (let i = 0; i < nextMusics.length; i++) {
-        let music = nextMusics[i];
-        let title = music.querySelector(".music_title").textContent.trim();
-        let difficulty = switchNumToDiff(Number(music.querySelector('input[type="hidden"][name="diff"]').getAttribute("value")));
-        let score = Number(music.querySelector(".play_musicdata_highscore").querySelector("span").textContent.replaceAll(",", ""));
+    // 新曲枠
+    progressCount++;
+    progressElement.innerHTML = engMode ? `<p>(${String(progressCount)}/${String(fullProgressCount)}) Fetching new songs...</p>` : `<p>(${String(progressCount)}/${String(fullProgressCount)}) 新曲枠楽曲を取得しています...</p>`;
+    const ratingsNewDoc = await fetchPageDoc(ratingsNewUrl);
+    const newMusics = ratingsNewDoc.getElementsByTagName("form");
+    const newMusicData = [];
+    for (let i = 0; i < newMusics.length; i++) {
+        const music = newMusics[i];
+        const title = music.querySelector(".music_title").textContent.trim();
+        const difficulty = switchNumToDiff(Number(music.querySelector('input[type="hidden"][name="diff"]').getAttribute("value")));
+        const score = Number(music.querySelector(".play_musicdata_highscore").querySelector("span").textContent.replaceAll(",", ""));
 
         // AJ/FCの照合
         let isAllJustice = false;
         let isFullCombo = false;
-        let musicInMusicData = musicData.filter(m => m.title === title && m.difficulty === difficulty && m.score === score)[0];
+        let fullChainLv = 0;
+        const musicInMusicData = musicData.filter(m => m.title === title && m.difficulty === difficulty && m.score === score)[0];
         if (musicInMusicData) {
             isAllJustice = musicInMusicData.isAllJustice;
             isFullCombo = musicInMusicData.isFullCombo;
+            fullChainLv = musicInMusicData.fullChainLv;
         }
+        const idx = Number.parseInt(music.querySelector('input[type="hidden"][name="idx"]').getAttribute("value"));
 
-        nextMusicData.push({
+        newMusicData.push({
             title: title,
             difficulty: difficulty,
             score: score,
             isAllJustice: isAllJustice,
-            isFullCombo: isFullCombo
+            isFullCombo: isFullCombo,
+            fullChainLv: fullChainLv,
+            idx: idx
         });
     }
     await sleep(1000);
 
-    UiBase.innerHTML += engMode ? "<p>Finished!</p>" : `<p>完了しました！</p>`;
-    // UiBase.innerHTML += `<button id="closer">${engMode ? "Close" : "閉じる"}</button>`
-    const closer = UiBase.appendChild(document.createElement("button"));
-    closer.id = "closer";
-    closer.textContent = engMode ? "Close" : "閉じる";
-    closer.style = "display: block; margin: 0 auto; padding: 5px 10px; font-size: 1.2rem; border: none; border-radius: 5px; background-color: rgb(13, 155, 13); color: #fff; cursor: pointer;"
-    UiBase.querySelector("#closer").addEventListener("click", () => UiBase.remove());
+    progressElement.style.textAlign = "center";
+    progressElement.innerHTML = engMode ?
+        "<p>Completed!</p><p>Click on the links below to load data directly into the tool.</p>" :
+        "<p>完了しました！</p><p>以下のリンクをクリックすると、ツールに直接データが読み込まれます。</p>";
+
     playerData.best = bestMusicData;
-    playerData.recent = recentMusicData;
-    playerData.candidate = nextMusicData;
+    playerData.new = newMusicData;
     playerData.score = musicData;
+
     const now = new Date();
+
+    // CHUNITHM Best Songs Image Generator NEW
+    const b30Wrapper = progressElement.appendChild(document.createElement("p"));
+    const b30 = b30Wrapper.appendChild(document.createElement("a"));
+    b30.textContent = "CHUNITHM Best Songs Image Generator NEW";
+    b30.style = "text-decoration: underline; cursor: pointer;";
+    b30.addEventListener("click", () => {
+        window.open(`https://reiwa.f5.si/newbestimg/chunithm_int?b=${String(Math.floor(now.getTime() / 1000))}`);
+    });
+
+    // CHUNITHM MY CONSTANTS
+    const mcWrapper = progressElement.appendChild(document.createElement("p"));
+    const mc = mcWrapper.appendChild(document.createElement("a"));
+    mc.textContent = "CHUNITHM MY CONSTANTS";
+    mc.style = "text-decoration: underline; cursor: pointer;";
+    mc.addEventListener("click", () => {
+        window.open(`https://reiwa.f5.si/newmyconstants/chunithm-int?b=${String(Math.floor(now.getTime() / 1000))}`);
+    });
+
+    // // テスト用リンク
+    // const testWrapper = progressElement.appendChild(document.createElement("p"));
+    // const test = testWrapper.appendChild(document.createElement("a"));
+    // test.textContent = "テスト用リンク";
+    // test.style = "text-decoration: underline; cursor: pointer;";
+    // test.addEventListener("click", () => {
+    //     window.open("https://qman11010101.github.io/postmessage_practice?b=" + String(Math.floor(now.getTime() / 1000)));
+    // });
+
+    window.addEventListener("message", (e) => {
+        if (e.origin !== "https://qman11010101.github.io" && e.origin !== "https://reiwa.f5.si") return;
+        if (e.data !== "ready") return;
+        e.source.postMessage(JSON.stringify(playerData), e.origin);
+    })
+
+    // ダウンロードリンク
     const blob = new Blob([JSON.stringify(playerData)], { type: "application/json" });
     const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = `chunithm-player-data_${String(Math.floor(now.getTime() / 1000))}.json`;
-    a.click();
+    const downloaderWrapper = progressElement.appendChild(document.createElement("p"));
+    const downloader = downloaderWrapper.appendChild(document.createElement("a"));
+    downloader.textContent = engMode ? "Download data as a JSON file" : "データをJSON形式でダウンロードする";
+    downloader.href = url;
+    downloader.download = `chunithm-player-data_${String(Math.floor(now.getTime() / 1000))}.json`;
+
+    const buttonStyle = "display: block; margin: 10px auto; padding: 5px 10px; font-size: 1.2rem; border: none; border-radius: 5px; background-color: rgb(13, 155, 13); color: #fff; cursor: pointer;";
+
+    // 閉じるボタン
+    const closer = progressElement.appendChild(document.createElement("button"));
+    closer.textContent = engMode ? "Close" : "閉じる";
+    closer.style = buttonStyle;
+    closer.addEventListener("click", () => UiBase.remove());
 }
 
 main();
