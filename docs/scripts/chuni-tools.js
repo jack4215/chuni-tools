8f32092a95c63cf829e70ade41d06c13c1589b7d
Jack Lu <87149267+jack4215@users.noreply.github.com>
2025-06-11 15:30:18 +0800

    update

--- docs/scripts/chuni-tools.js
+++ docs/scripts/chuni-tools.js
@@ -22,7 +22,7 @@
                 wrongBase: `請到 CHUNITHM-NET Intl. (${e}) 登入之後再試一次。`,
                 containerHTML: `
                     <p><a href="https://chuni.tsaibee.org/?lang=zh_TW" target="_blank" style="color: #75b4f7;text-decoration: none;">CHUNITHM Tools</a></p>
-                    <p style="font-size:17px;"><a href="https://chuni-event.tsaibee.org/" target="_blank">賽事平台</a> ｜ <a href="https://chuni.tsaibee.org/statistics" target="_blank">使用者統計</a> ｜ <a href="https://chuni.tsaibee.org/privacy-policy" target="_blank">隱私權政策</a></p>
+                    <p style="font-size:17px;"><a href="https://chuni-event.tsaibee.org/" target="_blank">賽事平台</a>　｜　<a href="https://chuni.tsaibee.org/statistics" target="_blank">使用者統計</a>　｜　<a href="https://chuni.tsaibee.org/privacy-policy" target="_blank">隱私權政策</a></p>
                 `
             }
         }[function() {
