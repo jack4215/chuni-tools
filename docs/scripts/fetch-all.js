2871410610ffaefb3aa3b85f2597b9e93322b7ec
Jack Lu <87149267+jack4215@users.noreply.github.com>
2025-07-21 17:20:20 +0800

    update

--- docs/scripts/fetch-all.js
+++ docs/scripts/fetch-all.js
@@ -402,7 +402,27 @@
                                 a.append("token", r("_t"));
                                 const n = await i("/mobile/record/musicGenre/sendMusicDetail/", a)
                                   , l = n.querySelectorAll(`.music_box.bg_${Object.entries(o).find((e => e[1] === t))[0]} .box14 > div`)[1].querySelector(".text_b")?.innerHTML.replace("times", "");
-                                return parseInt(l)
+                                return parseInt(l ?? "0")
+                            }(t.data.idx, t.data.difficulty)
+                            break;
+                        case "worldRank":
+                            s = async function(e, t) {
+                                const a = new FormData;
+                                a.append("idx", e);
+                                a.append("genre", "99");
+                                a.append("diff", c.indexOf(t).toString());
+                                a.append("category", "1");
+                                a.append("token", r("_t"));
+                                const n = await i("/mobile/ranking/sendRankingDetail/", a);
+                                const l = n.querySelector(".rank_block_inner_mine");
+                                if (l) {
+                                    const rank = l.querySelector(".rank_block_rank")?.textContent.trim() || "-";
+                                    console.log("%c    Your world rank: %c" + rank, "color: gray", "color: white");
+                                    return parseInt(rank);
+                                } else {
+                                    console.log("%c    Your world rank: %c-", "color: gray", "color: white");
+                                    return "-";
+                                }
                             }(t.data.idx, t.data.difficulty)
                         }
                         l("ping", {
