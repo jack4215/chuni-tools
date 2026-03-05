456a5b5df29adfaebcd200152a04cbf4f4546279
Jack Lu <87149267+jack4215@users.noreply.github.com>
2026-03-05 16:42:24 +0800

    update

--- docs/scripts/chuni-tools.js
+++ docs/scripts/chuni-tools.js
@@ -13,7 +13,8 @@
                 alreadyRun: "Please refresh the page before running CHUNI TOOLS again!",
                 wrongBase: `Please open CHUNITHM-NET Intl. (${e}) and login, then try again.`,
                 containerHTML: `
-                    <p><a href="https://chuni.tsaibee.org/constupdate" target="_blank">X-VERSE-X Chart Constant Update List</a></p>
+                    <p>B50 image download feature is now available.</p>
+                    <p>Go to Record viewer and click the download button at the top.</p>
                     <hr width="96%">
                     <p><a href="https://chuni.tsaibee.org/?lang=en_US" target="_blank" style="color: #75b4f7;text-decoration: none;">CHUNITHM Tools</a></p>
                     <p style="font-size:17px;"><a href="https://chuni-event.tsaibee.org/en/" target="_blank">Event Platform</a> ｜ <a href="https://chuni.tsaibee.org/statistics" target="_blank">User Stats</a> ｜ <a href="https://chuni.tsaibee.org/privacy-policy" target="_blank">Privacy Policy</a></p>
@@ -23,7 +24,8 @@
                 alreadyRun: "請重新整理頁面後再執行 CHUNITHM 書籤工具！",
                 wrongBase: `請到 CHUNITHM-NET Intl. (${e}) 登入之後再試一次。`,
                 containerHTML: `
-                    <p><a href="https://chuni.tsaibee.org/constupdate" target="_blank">X-VERSE-X 譜面定數更新列表</a></p>
+                    <p>下載 B50 圖片功能已開放</p>
+                    <p>進入查分器點擊上方下載按鈕即可儲存</p>
                     <hr width="96%">
                     <p><a href="https://chuni.tsaibee.org/?lang=zh_TW" target="_blank" style="color: #75b4f7;text-decoration: none;">CHUNITHM Tools</a></p>
                     <p style="font-size:17px;"><a href="https://chuni-event.tsaibee.org/" target="_blank">賽事平台</a>　｜　<a href="https://chuni.tsaibee.org/statistics" target="_blank">使用者統計</a>　｜　<a href="https://chuni.tsaibee.org/privacy-policy" target="_blank">隱私權政策</a></p>
@@ -59,7 +61,7 @@
                     background-color:rgb(34, 51, 68);
                     margin: 10px auto;
                     width: 440px;
-                    font-family: Arial, sans-serif;
+                    font-family: "Noto Sans TC" ,"Microsoft JhengHei", Arial, sans-serif;
                     border-radius: 4px;
                     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
                 }
