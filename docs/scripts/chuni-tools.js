6d61870fa3f6e5a0284adcc27f195c6e0b40e632
Jack Lu <87149267+jack4215@users.noreply.github.com>
2026-03-26 12:46:11 +0800

    update

--- docs/scripts/chuni-tools.js
+++ docs/scripts/chuni-tools.js
@@ -13,7 +13,7 @@
                 alreadyRun: "Please refresh the page before running CHUNI TOOLS again!",
                 wrongBase: `Please open CHUNITHM-NET Intl. (${e}) and login, then try again.`,
                 containerHTML: `
-                    <p>CHUNILIB Website is available!</p>
+                    <p>CHUNILIB Website is Launch!</p>
                     <p><a href="https://chunilib.tsaibee.org/" target="_blank">【Go to CHUNILIB】</a></p>
                     <hr width="96%">
                     <p><a href="https://chuni.tsaibee.org/?lang=en_US" target="_blank" style="color: #75b4f7;text-decoration: none;">CHUNITHM Tools</a></p>
@@ -24,7 +24,7 @@
                 alreadyRun: "請重新整理頁面後再執行 CHUNITHM 書籤工具！",
                 wrongBase: `請到 CHUNITHM-NET Intl. (${e}) 登入之後再試一次。`,
                 containerHTML: `
-                    <p>CHUNILIB 成績保存網站上線！</p>
+                    <p>CHUNILIB 成績管理網站上線！</p>
                     <p><a href="https://chunilib.tsaibee.org/" target="_blank">【前往 CHUNILIB】</a></p>
                     <hr width="96%">
                     <p><a href="https://chuni.tsaibee.org/?lang=zh_TW" target="_blank" style="color: #75b4f7;text-decoration: none;">CHUNITHM Tools</a></p>
