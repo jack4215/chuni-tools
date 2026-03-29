18de28419ccb4363a8280cb81052bf6bda432906
Jack Lu <87149267+jack4215@users.noreply.github.com>
2026-03-29 16:32:18 +0800

    recover

--- docs/scripts/fetch-all.js
+++ docs/scripts/fetch-all.js
@@ -95,14 +95,7 @@
                 r.href = t("fetch-all") + "/common/styles/inject.css",
                 e.innerText = s.analyzeRating,
                 e.href = t("fetch-all") + "/record-viewer/#all",
-                e.addEventListener("click", (evt => {
-                    evt.preventDefault();
-                    if (window.chuniViewer && !window.chuniViewer.closed) {
-                        window.chuniViewer.focus();
-                    } else {
-                        window.chuniViewer = window.open(e.href, "_blank");
-                    }
-                })),
+                e.target = "recordViewer",
                 l.getElementsByTagName("head")[0].appendChild(r),
                 r.addEventListener("load", ( () => {
                     l.querySelector(".clearfix")?.insertAdjacentElement("afterend", e);
