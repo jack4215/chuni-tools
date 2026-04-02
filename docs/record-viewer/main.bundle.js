f54a4f7f53d02aa4cdd466034a5e13892d1f97d3
Jack Lu <87149267+jack4215@users.noreply.github.com>
2026-04-02 15:51:07 +0800

    20

--- docs/record-viewer/main.bundle.js
+++ docs/record-viewer/main.bundle.js
@@ -5408,7 +5408,7 @@
         setTimeout(() => {
           const hasGid = !!localStorage.getItem("chuni_gid");
           const msg = hasGid ? d(wt)("record.fetch.loggedin") : d(wt)("record.fetch.notloggedin");
-          const displayTime = hasGid ? 6000 : 20000;
+          const displayTime = hasGid ? 6000 : 60000;
           const toast = document.createElement("div");
           toast.style.cssText = ` position: fixed; z-index: 9999; bottom: 0.5rem; left: 0.5rem; padding: 0.5rem; border-radius: 0.5rem; background-color: rgba(0, 0, 0, 0.533); backdrop-filter: blur(2px); transform: translateY(100px); opacity: 0; transition: transform 0.4s cubic-bezier(0.1, 0.8, 0.2, 1), opacity 0.4s ease; `;
           toast.innerHTML = `<span style="color: var(--theme-text-dim); font-size: 1em;">${msg}</span>`;
