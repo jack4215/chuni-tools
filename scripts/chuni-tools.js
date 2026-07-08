482ea042dde20b877008199d2f65ad0e5270155d
Jack Lu <87149267+jack4215@users.noreply.github.com>
2026-07-08 17:07:48 +0800

    add banner

--- scripts/chuni-tools.js
+++ scripts/chuni-tools.js
@@ -15,6 +15,8 @@
                 containerHTML: `
                     <p><a href="https://chunilib.tsaibee.org/constupdate" target="_blank">CHUNITHM Mate Const Update >></a></p>
                     <hr width="96%">
+                    <a href="https://chunilib.tsaibee.org" target="_blank"><img src="https://chuni.tsaibee.org/data/banner_en.jpg" style="width: 96%;"></a>
+                    <hr width="96%">
                     <p><a href="https://chuni.tsaibee.org/?lang=en_US" target="_blank" style="color: #75b4f7;text-decoration: none;">CHUNITHM Tools</a></p>
                     <p style="font-size:17px;"><a href="https://chunilib.tsaibee.org/" target="_blank">CHUNILIB</a> ｜ <a href="https://chuni.tsaibee.org/statistics" target="_blank">User Stats</a> ｜ <a href="https://chuni.tsaibee.org/privacy-policy" target="_blank">Privacy Policy</a></p>
                 `
@@ -24,6 +26,8 @@
                 wrongBase: `請到 CHUNITHM-NET Intl. (${e}) 登入之後再試一次。`,
                 containerHTML: `
                     <p><a href="https://chunilib.tsaibee.org/constupdate" target="_blank">CHUNITHM Mate 定數更新列表 >></a></p>
+                    <hr width="96%">
+                    <a href="https://chunilib.tsaibee.org/?lang=zh_TW" target="_blank"><img src="https://chuni.tsaibee.org/data/banner_zh.jpg" style="width: 96%;"></a>
                     <hr width="96%">
                     <p><a href="https://chuni.tsaibee.org/?lang=zh_TW" target="_blank" style="color: #75b4f7;text-decoration: none;">CHUNITHM Tools</a></p>
                     <p style="font-size:17px;"><a href="https://chunilib.tsaibee.org/?lang=zh_TW" target="_blank">CHUNILIB</a>　｜　<a href="https://chuni.tsaibee.org/statistics" target="_blank">使用者統計</a>　｜　<a href="https://chuni.tsaibee.org/privacy-policy" target="_blank">隱私權政策</a></p>
